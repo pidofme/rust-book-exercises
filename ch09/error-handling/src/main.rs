b4d926649038ae55d9a5e27ea8867262d232b9ef
pidofme <pidofme@gmail.com>
2022-11-04 21:52:01 +0800

    ch09 done

---
+++ ch09/error-handling/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opeing the file: {:?}", other_error);
+            }
+        },
+    };
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let username_file_result = File::open("hello.txt");
+
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut username = String::new();
+
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username = String::new();
+
+//     File::open("hello.txt")?.read_to_string(&mut username)?;
+
+//     Ok(username)
+// }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
